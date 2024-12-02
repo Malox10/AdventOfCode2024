7fdea30947bf4b1e27019774490f15beb5c5e965
Malox <Maxi.Junges@gmx.de>
2024-12-02 18:27:04 +0100

    Day01

---
+++ src/Day01.kt
@@ -0,0 +1,42 @@
+import kotlin.math.absoluteValue
+
+fun main() {
+    val name = "Day01"
+    fun parse(input: List<String>): Pair<List<Int>, List<Int>> {
+        val pairs = input.map { line ->
+            line.split(" ")
+                .filter { it.isNotEmpty() }
+                .map { it.trim().toInt() }
+                .let { (a, b) -> a to b }
+        }
+
+        return pairs.map { it.first } to pairs.map { it.second }
+    }
+
+    fun part1(input: List<String>): Int {
+        val (left, right) = parse(input)
+
+        return left.sorted()
+            .zip(right.sorted())
+            .sumOf { (a, b) -> (a - b).absoluteValue }
+    }
+
+    fun part2(input: List<String>): Int {
+        val (left, right) = parse(input)
+            .map { list -> list.toSet().associateWith { number -> list.count { it == number } } }
+
+        return left.entries.sumOf { (key, count) -> key * count * (right[key] ?: 0) }
+    }
+
+
+
+    // Or read a large test input from the `src/Day01_test.txt` file:
+    val testInput = readInput("${name}Test")
+    check(part1(testInput) == 11)
+    check(part2(testInput) == 31)
+
+    // Read the input from the `src/Day01.txt` file.
+    val input = readInput(name)
+    part1(input).println()
+    part2(input).println()
+}
