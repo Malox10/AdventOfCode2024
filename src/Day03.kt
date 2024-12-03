a31a88467a4935730c5c8f7d4e9f4fd73a253c6e
Malox <Maxi.Junges@gmx.de>
2024-12-03 15:45:19 +0100

    Day03 Part2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,33 +1,35 @@
 fun main() {
     val name = "Day03"
-    fun parse(input: List<String>) {
 
-    }
+    val baseRegex = """mul\((\d{1,3}),(\d{1,3})\)"""
+    fun List<String>.findScore() = this.map { it.toInt() }.let { (a, b) -> a * b }
+    fun List<String>.findTotalScore(regex: Regex) =
+        this.flatMap { regex.findAll(it) }.sumOf { it.groupValues.drop(1).findScore() }
 
     fun part1(input: List<String>): Int {
-        val regex = Regex("""mul\((\d{1,3}),(\d{1,3})\)""")
-
-        return input
-            .flatMap { regex.findAll(it) }
-            .sumOf {
-                it.groupValues.drop(1).map { group ->
-                    group.toInt()
-                }.let { (a, b) -> a * b }
-            }
+        val regex = Regex(baseRegex)
+        return input.findTotalScore(regex)
     }
 
     fun part2(input: List<String>): Int {
-        val x = parse(input)
-        return input.size
+        val regex = Regex("""don't\(\)|do\(\)|$baseRegex""")
+        var enabled = true
+
+        return regex.findAll(input.joinToString()).sumOf{ match ->
+            when (match.value) {
+                "don't()" -> enabled = false
+                "do()" -> enabled = true
+                else -> if (enabled) return@sumOf match.groupValues.drop(1).findScore()
+            }
+            0
+        }
     }
-
-
 
     val testInput = readInput("${name}Test")
     check(part1(testInput) == 161)
-//    check(part2(testInput) == 1)
+    check(part2(listOf("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")) == 48)
 
     val input = readInput(name)
-    part1(input).println()
-    part2(input).println()
+    "part1: ${part1(input)}".println()
+    "part2: ${part2(input)}".println()
 }
