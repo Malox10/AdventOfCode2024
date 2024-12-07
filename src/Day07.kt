b8d055e3e56ac227fb9738524436fe23c212e562
Malox <Maxi.Junges@gmx.de>
2024-12-07 12:28:17 +0100

    Day7 Part2

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,5 +1,8 @@
 fun main() {
-    data class Equation(val target: Long, val operands: List<Long>)
+    data class Equation(val target: Long, val operands: List<Long>) {
+        fun dropFirst() = this.copy(operands = operands.drop(1))
+    }
+
     fun parse(input: List<String>): List<Equation> {
         return input.map { line ->
             val (result, operands) = line.split(":").map { it.trim() }
@@ -9,42 +12,37 @@ fun main() {
         }
     }
 
-    val operators = listOf(
+    val operatorsPart1 = listOf(
         { a: Long, b: Long -> a * b },
         { a: Long, b: Long -> a + b },
     )
 
-
-
-    fun evaluateRecursive(operands: List<Long>, accumulator: Long): List<Long> {
-        if(operands.isEmpty()) return listOf(accumulator)
-        return operators.flatMap { operation ->
-            val result = operation(accumulator, operands.first())
-            evaluateRecursive(operands.drop(1), result)
+    fun Equation.findCombinations(accumulator: Long, operators: List<(Long, Long) -> Long>): Boolean {
+        if (this.operands.isEmpty()) return this.target == accumulator
+        return operators.any { operation ->
+            val result = operation(accumulator, this.operands.first())
+            this.dropFirst().findCombinations(result, operators)
         }
     }
 
-    fun evaluate(operands: List<Long>) = evaluateRecursive(operands.drop(1), operands.first())
-
+    fun Equation.hasCombination(operators: List<(Long, Long) -> Long> = operatorsPart1) =
+        this.dropFirst().findCombinations(this.operands.first(), operators)
 
     fun part1(input: List<String>): Long {
         val equations = parse(input)
-        return equations.sumOf { equation ->
-            val possibleResults = evaluate(equation.operands)
-            if(possibleResults.contains(equation.target)) equation.target else 0
-        }
+        return equations.filter { it.hasCombination() }.sumOf { it.target }
     }
 
     fun part2(input: List<String>): Long {
-        val x = parse(input)
-        return input.size.toLong()
+        val equations = parse(input)
+        val allOperators = operatorsPart1.plus { a: Long, b: Long -> "$a$b".toLong() }
+
+        return equations.filter { it.hasCombination(allOperators) }.sumOf { it.target }
     }
-
-
 
     val testInput = readInput("Day07Test")
     checkDebug(part1(testInput), 3749)
-//    checkDebug(part2(testInput), 1)
+    checkDebug(part2(testInput), 11387)
 
     val input = readInput("Day07")
     "part1: ${part1(input)}".println()
