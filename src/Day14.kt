565166d0381627966f7fe614fd881c7dbd2ce438
Malox <Maxi.Junges@gmx.de>
2024-12-15 01:21:23 +0100

    Day14 Part2

--- src/Day14.kt
+++ src/Day14.kt
@@ -23,6 +23,12 @@ fun main() {
     }
 
 //    fun generateGrid() = List(103) { MutableList(101) { emptyList<Guard>().toMutableList() } }
+
+    fun printArray(guards: List<Guard>, width: Int, height: Int) {
+        val array = Array(height) { Array(width) { 0 } }
+        guards.forEach { guard -> array[guard.position.first][guard.position.second]++ }
+        array.forEach { row -> row.joinToString("") { if (it > 0) "█" else " " }.println() }
+    }
     fun part1(input: List<String>, width: Int = 101, height: Int = 103): Int {
         val guards = parse(input)
         repeat(100) {
@@ -42,20 +48,40 @@ fun main() {
                 if(guard.position.second < col) list[2]++
                 if(guard.position.second > col) list[3]++
             }
-
-            println(guard)
         }
 
-        val array = Array(height) { Array(width) { 0 } }
-        guards.forEach { guard -> array[guard.position.first][guard.position.second]++ }
-        array.forEach { it.contentToString().println() }
-
+        printArray(guards, width, height)
         return list.reduce(Int::times)
     }
 
-    fun part2(input: List<String>): Int {
-        val x = parse(input)
-        return input.size
+    fun checkKernel(guards: List<Guard>, width: Int, height: Int): Boolean {
+        val array = Array(height) { Array(width) { 0 } }
+        guards.forEach { guard -> array[guard.position.first][guard.position.second]++ }
+
+        for(row in array.indices) {
+            loop@ for(col in array.first().indices) {
+                for(i in 1..5) {
+                    for(j in 1..5) {
+                        if((array.getOrNull(row + i)?.getOrNull(col + j) ?: 0) == 0) continue@loop
+                    }
+                }
+                return true
+            }
+        }
+
+        return false
+    }
+    fun part2(input: List<String>, width: Int = 101, height: Int = 103): Int {
+        val guards = parse(input)
+        var counter = 0
+        while(true) {
+            counter++
+            guards.forEach { it.move(width, height) }
+            if(checkKernel(guards, width, height)) {
+                println("index: $counter")
+                printArray(guards, width, height)
+            }
+        }
     }
 
 
