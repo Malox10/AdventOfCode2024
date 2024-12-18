412a8bec0f7e6db803984e170ce2d03d94eded97
Malox <Maxi.Junges@gmx.de>
2024-12-18 12:47:20 +0100

    Day18 Part1

---
+++ src/Day18.kt
@@ -0,0 +1,66 @@
+import java.util.PriorityQueue
+
+fun main() {
+    fun parse(input: List<String>): List<Point> {
+        return input.map { line -> line.split(",").map { it.trim().toInt() }.let { (a, b) -> a to b } }
+    }
+
+
+    data class Node(val point: Point, val steps: Int, val score: Int)
+    fun getScore(location: Point, end: Point, steps: Int): Int {
+        val delta = end - location
+        return delta.first + delta.second + steps
+    }
+    fun Node.getNeighbours(errors: Set<Point>, end: Point): List<Node> {
+        return Direction.entries.mapNotNull { direction ->
+            val newDirection = point + direction.offset
+            if(errors.contains(newDirection)) return@mapNotNull null
+
+            if(newDirection.first < 0 || newDirection.second < 0) return@mapNotNull null
+            if(newDirection.first > end.first || newDirection.second > end.second) return@mapNotNull null
+            Node(newDirection, steps + 1, getScore(newDirection, end, steps))
+        }
+    }
+
+    fun aStar(errors: Set<Point>, end: Point = 70 to 70): Int {
+        val priorityQueue = PriorityQueue<Node>(compareBy { it.score })
+        priorityQueue.add(Node(Point(0, 0), 0, 0))
+        val closedNodes = mutableMapOf<Point, Int>()
+
+        while(priorityQueue.isNotEmpty()) {
+            val node = priorityQueue.remove()
+            if(node.point == end) return node.steps
+            val neighbours = node.getNeighbours(errors, end)
+                .filter { node ->
+                    val value = closedNodes[node.point] ?: return@filter true
+                    value > node.steps
+                }
+            //add filtering by closedNodes here
+            priorityQueue.addAll(neighbours)
+            closedNodes[node.point] = node.steps
+        }
+
+        throw Error("no finish found")
+    }
+
+    fun part1(input: List<String>, end: Point = 70 to 70, errorCount: Int = 1024): Int {
+        val errors = parse(input).take(errorCount)
+        return aStar(errors.toSet(), end)
+    }
+
+    fun part2(input: List<String>): Int {
+        val x = parse(input)
+        return input.size
+    }
+
+
+
+    val testInput = readInput("Day18Test")
+    checkDebug(part1(testInput, 6 to 6, 12), 22)
+//    checkDebug(part2(testInput), 1)
+    println("test done")
+
+    val input = readInput("Day18")
+    "part1: ${part1(input)}".println()
+    "part2: ${part2(input)}".println()
+}
