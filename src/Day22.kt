d58c10a4ab68508e545ad7003721d26367ad2369
Malox <Maxi.Junges@gmx.de>
2024-12-22 12:06:16 +0100

    Day22 Part2

--- src/Day22.kt
+++ src/Day22.kt
@@ -35,15 +35,36 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        val x = parse(input)
-        return input.size
-    }
+        val secrets = parse(input)
+        val map = mutableMapOf<List<Int>, Int>()
 
+        secrets.forEach { secret ->
+            val secretChanges = mutableMapOf<List<Int>, Int>()
+            (1..2000)
+                .runningFold(secret) { acc, _ -> acc.nextSecret() }
+                .windowed(5)
+                .forEach { window ->
+                    val deltas =
+                        window.windowed(2).map { (a, b) -> "$b".last().digitToInt() - "$a".last().digitToInt() }
+                    val value = window.last().toString().last().digitToInt()
+                    secretChanges.putIfAbsent(deltas, value)
+                }
+
+            secretChanges.forEach { (changes, value) ->
+                val readValue = map[changes]
+                map[changes] = (readValue ?: 0) + value
+            }
+        }
+
+        return map.maxBy { it.value }.value
+    }
 
 
     val testInput = readInput("Day22Test")
     checkDebug(part1(testInput), 37327623)
-//    checkDebug(part2(testInput), 1)
+
+    val testInput2 = readInput("Day22Test2")
+    checkDebug(part2(testInput2), 23)
 
     val input = readInput("Day22")
     "part1: ${part1(input)}".println()
